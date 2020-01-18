8ce0446612d67b7092193e15e77d14b4b52e4c3e
newsun87 <newsun87@mail.sju.edu.tw>
2020-01-18 17:04:17 +0800

    init.

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ import json
 import random
 import time
 import pyperclip
-import clipboard
 
 access_token = "6gOhUQlcO8tQkdUfaw369cspUy378X9lMbJV8nuyGYbcRDYNRJy3N9SvRXjkrbxRBtGCga9hSH6CK+pZtJzam5b4GCExt3QWIbV5MZkgcnTTWa8VemIzchGty8Jhkw2SP8gL6Q7mMD8udCaBJ+icmwdB04t89/1O/w1cDnyilFU="
 channel_secret = "8b5c6e8e8df7c5859562f60407602970"
