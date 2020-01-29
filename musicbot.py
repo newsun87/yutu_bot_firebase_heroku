ee095e48b868fffdb30b539808a5eeee240d653a
newsun87 <newsun87@mail.sju.edu.tw>
2020-01-29 16:37:43 +0800

    init.

--- musicbot.py
+++ musicbot.py
@@ -6,9 +6,7 @@ from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
