87d7283c7b9c597d2fea5bf8aa7ea11a33138812
tinaliou4 <tinaliou4@gmail.com>
2021-02-22 11:14:49 +0000

    init.

--- youtu-musicbot.py
+++ youtu-musicbot.py
@@ -268,11 +268,11 @@ def handle_postback_message(event):
     video_url = postBack_msg.split("~")[1]
     songname = postBack_msg.split("~")[2]
     userId = postBack_msg.split("~")[3]
-    print(video_url, songname, userId)
+    print(video_url, songname, userId, base_users_userId)
     if action == 'mqtt_publish':
-      ref.child(base_users_userId + userId + '/youtube_music/').update({"songkind":songname})         
-      ref.child(base_users_userId + userId + '/youtube_music/').update({"videourl":video_url})
-      print("歌曲 {videourl} 更新成功...".format(videourl=video_url)
+      #ref.child(base_users_userId + userId + '/youtube_music/').update({"songkind":songname})         
+      #ref.child(base_users_userId + userId + '/youtube_music/').update({"videourl":video_url})
+      #print("歌曲 {videourl} 更新成功...".format(videourl=video_url)
       #client.publish("music/youtubeurl", userId +'~'+ video_url, 2, retain=True) #發佈訊息 
       #print("message published")
       line_bot_api.reply_message(event.reply_token, TextSendMessage(text=video_url)) 
