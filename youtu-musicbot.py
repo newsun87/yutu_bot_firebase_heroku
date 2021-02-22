e71743df811d3d9b167e05a3105b35391394b16a
tinaliou4 <tinaliou4@gmail.com>
2021-02-22 11:22:55 +0000

    init.

--- youtu-musicbot.py
+++ youtu-musicbot.py
@@ -273,8 +273,10 @@ def handle_postback_message(event):
       ref.child(base_users_userId + userId + '/youtube_music/').update({"songkind":songname})         
       ref.child(base_users_userId + userId + '/youtube_music/').update({"videourl":video_url})
       #print("歌曲 {videourl} 更新成功...".format(videourl=video_url)
-      #client.publish("music/youtubeurl", userId +'~'+ video_url, 2, retain=True) #發佈訊息 
-      #print("message published")
+      client.publish("music/youtubeurl", userId +'~'+ video_url, 2, retain=True) #發佈訊息 
+      print("message published")
+      time.sleep(1)
+      client.publish("music/youtubeurl", '', 2, retain=True) #發佈訊息  
       line_bot_api.reply_message(event.reply_token, TextSendMessage(text=video_url)) 
        
 def linenotify_menu():
